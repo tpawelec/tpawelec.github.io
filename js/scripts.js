75e08f54c1893dcfeb6b776bd674581f032db189
Tomasz Pawelec <30554524+lilinho@users.noreply.github.com>
2017-12-01 17:05:48 +0100

    scroll in progress

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,32 @@ $("#navList li").click(function(e) {
 	$('#navList #' + String(currElem)).removeClass("active");
 	$('#navList #' + String(tarElem)).addClass("active");
 
-	$('#pageContent #' + String(currElem) + 'Section').css('visibility', 'hidden');
-	$('#pageContent #' + String(tarElem) + 'Section').css('visibility', 'visible');
+	$('#pageContent #' + String(currElem) + 'Section').css('left', '-500rem');
+	$('#pageContent #' + String(tarElem) + 'Section').css('left', '0');
 	
 
+});
+    
+/*
+    SCROLL EVENT
+*/
+/*    
+$().scroll(function() {
+    var currentScroll = $(window).scrollTop();
+    console.log(currentScroll);
+    if(0 <= currentScroll < 100) {
+        console.log("1");
+    } else if (100 <= currentScroll < 200) {
+        console.log("2");
+    } else if (200 <= currentScroll < 300) {
+        console.log("3");
+    } else {
+        console.log("4");
+    }
+}); */
+    
+$('#pageContent').bind('mousewheel', function(e) {
+    var delta = e.originalEvent.wheelDelta;
+    console.log(delta);
 });
 });
