da364cd2158a9f2744fa1b3d0ca1fffd97357422
Tomasz Pawelec <30554524+lilinho@users.noreply.github.com>
2017-12-03 16:43:51 +0100

    projects still to do

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,63 @@
-$(function() {
+$(function () {
 
     function getCurrenElementDiv() {
         var currElem = $('#navList .active').attr("id");
-        
+
         return $('#pageContent #' + String(currElem) + 'Section');
     }
-    
+
     function getCurrenElementNav() {
         var currElem = $('#navList .active').attr("id");
-        
+
         return $('#navList #' + String(currElem));
     }
-/*
-	MENU
-*/
+    /*
+    	MENU
+    */
 
-$("#navList li").click(function(e) {
-	var tarElem = e.currentTarget.id;
-	var currElemDiv = getCurrenElementDiv();
-    var currElemNav = getCurrenElementNav();
+    $("#navList li").click(function (e) {
+        var tarElem = e.currentTarget.id;
+        var currElemDiv = getCurrenElementDiv();
+        var currElemNav = getCurrenElementNav();
 
-    currElemNav.removeClass("active");
-	$('#navList #' + String(tarElem)).addClass("active");
+        currElemNav.removeClass("active");
+        $('#navList #' + String(tarElem)).addClass("active");
 
-	currElemDiv.css('left', '-200rem');
-	$('#pageContent #' + String(tarElem) + 'Section').css('left', '0');
-	
+        currElemDiv.css('left', '-200rem');
+        $('#pageContent #' + String(tarElem) + 'Section').css('left', '0');
 
-});
-    
-/*
-    SCROLL EVENT
-*/
-    
-$('#pageContent').on('mousewheel', function(e) {
-    var delta = e.originalEvent.wheelDeltaY;
-    var currElemDiv = getCurrenElementDiv();
-    var currElemNav = getCurrenElementNav();
-    
-    if(delta < 0) {
-        if(currElemDiv.attr("id") != "contactSection") {
-            currElemNav.removeClass("active");
-            currElemNav.next().addClass("active");
-        
-            currElemDiv.css('left', '-200rem');
-            currElemDiv.next().css('left', '0');
+
+    });
+
+    /*
+        SCROLL EVENT
+    */
+
+    $('#pageContent').on('mousewheel', function (e) {
+        var delta = e.originalEvent.wheelDelta;
+        var currElemDiv = getCurrenElementDiv();
+        var currElemNav = getCurrenElementNav();
+
+        if (delta < 0) {
+            if (currElemDiv.attr("id") != "contactSection") {
+                currElemNav.removeClass("active");
+                currElemNav.next().addClass("active");
+
+                currElemDiv.css('left', '-200rem');
+                currElemDiv.next().css('left', '0');
+            }
+        } else {
+            if (currElemDiv.attr("id") != "homeSection") {
+                currElemNav.removeClass("active");
+                currElemNav.prev().addClass("active");
+
+                currElemDiv.css('left', '-200rem');
+                currElemDiv.prev().css('left', '0');
+            }
         }
-    } else {
-        if(currElemDiv.attr("id") != "homeSection") {
-            currElemNav.removeClass("active");
-            currElemNav.prev().addClass("active");
-        
-            currElemDiv.css('left', '-200rem');
-            currElemDiv.prev().css('left', '0');
-        }
-    }
-    console.log(delta);
-    
-    
-});
+
+
+    });
+
+   
 });
