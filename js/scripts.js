4be2e06e582b732f8009301c4fa6674ea1d6c213
Tomasz Pawelec <tpawelec@outlook.com>
2019-07-18 19:30:25 +0200

    contact section in half way. anchors in nav todo.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
-const text = 'Hello';
+/*
+    Parallax
+*/
+const header = document.querySelector('.p-header');
+let screenHeight = window.innerHeight;
 
-for (let i = 0; i < 10; i++) {
-    console.log(text);
+function parallax() {
+    let scroll = window.scrollY;
+    let offset = header.getBoundingClientRect().bottom;
+    if(offset > screenHeight && offset) {
+        header.style.backgroundPosition = 'center ' + 'calc(' + (( distanceFromBottom  ) * 0.5) + 'px + 20%)';
+    } else {
+        header.style.backgroundPosition = 'center ' + 'calc(' + (( -scroll ) * 0.5) + 'px + 20%)';
+    }
+
 }
+
+window.addEventListener('scroll', parallax);
