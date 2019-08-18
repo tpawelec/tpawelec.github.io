2453de155c83671746eb2da460eb101158ed27b9
Tomasz Pawelec <tpawelec@outlook.com>
2019-08-18 16:44:55 +0200

    Done?

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function(grunt) {
         uglify: {
             my_target: {
                 files: {
-                    'js/scrits.min.js' : 'js/scripts.js'
+                    'js/scripts.min.js' : 'js/scripts.js'
                 }
             }
         },
